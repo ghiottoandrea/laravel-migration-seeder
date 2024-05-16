ce17cd3793cc906b2f510503d4cb9af0d480c3db
ghiottoandrea <andreaghiotto9@gmail.com>
2024-05-16 16:03:14 +0200

    first commit

---
+++ database/migrations/2024_05_16_135251_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Company');
+            $table->string('Departure_station');
+            $table->string('Arrival_station');
+            $table->string('Departure_time');
+            $table->string('Arrival_tIme');
+            $table->integer('Train_code');
+            $table->tinyInteger('Carriagers');
+            $table->boolean('In_time');
+            $table->boolean('Deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
